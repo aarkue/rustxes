b0341a2b9ba2d09b222b6a94f4b57e58c2845414
aarkue <mail@aarkue.eu>
2023-10-27 10:09:41 +0200

    Add simple test for easier execution

--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,27 @@
 #[cfg(test)]
 mod xes_tests {
+    use std::fs;
+
     use crate::xes_import::import_xes_file;
 
     #[test]
     fn basic_xes() {
-        let log = import_xes_file("/home/aarkue/dow/test.xes");
-        println!("{:#?}",log);}
+        let log = import_xes_file("../../../../dow/roadtraffic50traces.xes");
+        println!("{:#?}",log);
+    }
+
+    #[test]
+    fn pm4py_test_logs() {
+        let paths = fs::read_dir("../../pm4py-core/tests/input_data/").unwrap();
+        for path_el in paths {
+            let path = path_el.unwrap().path();
+            let path_str = path.to_str().unwrap();
+            if path_str.ends_with(".xes") || path_str.ends_with(".xes.gz") {
+                println!("Path: {}", path_str);
+                let _log = import_xes_file(path_str);
+                println!("Success!");
+                // println!("{:#?}",_log);
+            }
+        }
+    }
 }
